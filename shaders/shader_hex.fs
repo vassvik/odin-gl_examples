5874418ac4212910f177158239a225bc2a1a937c
vassvik <mvassvik@gmail.com>
2017-08-09 00:22:03 +0200

    typos

--- shaders/shader_hex.fs
+++ shaders/shader_hex.fs
@@ -5,9 +5,9 @@ flat in int instance;
 out vec4 color;
 
 void main() {
-	float R = 0.5 + 0.5*cos(1.3 + 2.0*instance);
-	float G = 0.5 + 0.5*cos(2.5 - 3.0*instance);
-	float B = 0.5 + 0.5*cos(3.2 + 5.0*instance);
+    float R = 0.5 + 0.5*cos(1.3 + 2.0*instance);
+    float G = 0.5 + 0.5*cos(2.5 - 3.0*instance);
+    float B = 0.5 + 0.5*cos(3.2 + 5.0*instance);
 
-	color = vec4(R, G, B, 1.0);
+    color = vec4(R, G, B, 1.0);
 }
